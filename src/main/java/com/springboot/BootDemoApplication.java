31930a8447f1dbfc9edd144d9d2c8f2e7976c36a
rakesh-kumar1 <rakesh.143t@gmail.com>
2018-01-24 12:28:50 +0530

    remove unused imports

--- src/main/java/com/springboot/BootDemoApplication.java
+++ src/main/java/com/springboot/BootDemoApplication.java
@@ -3,7 +3,6 @@ package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
